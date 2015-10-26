73dbb3110b97d8a870c3d73de0f4b9f6e47226ed
Ethan Dahlke <dahlke.ethan@gmail.com>
2015-10-26 00:28:58 -0500

    Basic Start
    start of event production equipment organization software.

---
+++ Audio.cs
@@ -0,0 +1,18 @@
+// Audio.cs
+// Created on October 25, 2015 by Ethan Dahlke
+// Last updated, October 25, 2015
+
+class Case : Equipment
+{
+	
+}
+
+class Rack : Equipment
+{
+	
+}
+
+class Speaker : Equipment
+{
+	
+}
---
+++ Equipment.cs
@@ -0,0 +1,37 @@
+// Equipment.cs
+// Created on October 25, 2015 by Ethan Dahlke
+// Last updated, October 25, 2015
+
+public class Equipment{
+	private string myID;
+	private int myWidth;
+	private int myLength:
+	private int myHeight;
+	private int myWeight;
+	
+	public string ID
+	{
+		get
+		{
+			return myID;
+		}
+		
+		set
+		{
+			myID = value; 
+		}
+	}
+	
+	public int Weight
+	{
+		get
+		{
+			return myWeight;
+		} 
+		
+		set
+		{
+			myWeight = value; 
+		}
+	}
+}
